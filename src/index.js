84c3fd4f1927d1dedef01f7bc6f8f94ea0d732f8
Ellielton <ellieltongama@gmail.com>
2024-08-27 17:20:38 -0300

    Temos uma classe personagem que mantem o modelo de objeto e agora criei uma classe personagemView que tem duas funções com a responsabilidade de contruir e redenrizar os personagens na tela

--- src/index.js
+++ src/index.js
@@ -3,4 +3,3 @@ import { Personagem } from "./modules/personagem.js"
 const personagemPedrinho = new Personagem('Pedrinho',5,'Mago')
 const personagemJose = new Personagem('Jose',3,'Arqueiro')
 
-console.log(personagemPedrinho.nome)
