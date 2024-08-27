83fe136858a9806c5282256960d1ce231d1ac97d
Ellielton <ellieltongama@gmail.com>
2024-08-27 16:51:06 -0300

    Criando um constructor dentro da classe. Instânciando um objeto passando suas características por parâmentros na instância.

--- src/index.js
+++ src/index.js
@@ -1,40 +1,6 @@
 import { Personagem } from "./modules/personagem.js"
+  
+const personagemPedrinho = new Personagem('Pedrinho',5,'Mago')
+const personagemJose = new Personagem('Jose',3,'Arqueiro')
 
-/*const personagemPedrinho = {
-    nome: 'Pedrinho',
-    vida: 7,
-    mana: 12,
-    level: 5,
-    tipo: 'Mago',
-}*/
-
-/*const personagemJose = {
-    nome: 'Jose',
-    vida: 7,
-    mana: 6,
-    level: 3,
-    tipo: 'Arqueiro',
-}*/
-   
-//Criando a personagem Ana 
-const personagemAna = {
-    nome: "Ana",
-    vida: 8,
-    mana: 10
-}
-
-
-const personagemPedrinho = new Personagem()
-personagemPedrinho.nome = "Pedrinho"
-personagemPedrinho.mana = 12
-personagemPedrinho.vida = 7
-personagemPedrinho.tipo = "Mago"
-personagemPedrinho.level = 5
-
-const personagemJose = new Personagem()
-personagemJose.nome = "José"
-personagemJose.tipo = "Arqueiro"
-personagemJose.level = 3
-
-console.log('Insiginia de ' + personagemPedrinho.nome + ': ' + personagemPedrinho.obterInsignia());
-console.log('Insiginia de ' + personagemJose.nome + ': ' + personagemJose.obterInsignia());
+console.log(personagemPedrinho.nome)
--- src/modules/personagem.js
+++ src/modules/personagem.js
@@ -7,6 +7,12 @@
   tipo
   descricao
 
+  constructor(nome, level, tipo) {
+    this.nome
+    this.level
+    this.tipo
+  }
+
   obterInsignia() {
     if(this.level >= 5) {
       return `Implacável ${this.tipo}`
