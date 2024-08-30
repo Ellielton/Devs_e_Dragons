69a35f71ca9784c9a12c4ec63616c8d42fe0e9da
Ellielton <ellieltongama@gmail.com>
2024-08-30 15:50:18 -0300

    Criando um novo personagem mesclando as propriedades do que já existem. Este tem um arquivo exclusivo no module que exportado para index.js, usa extends para acessar a propriedades do Personagem, nas suas proprias propriedades faz instâncias de outros personagens que seram mesclados: arqueiro e mago. e Ambos compartilham o mesmo método que trará resuldatos diferentes em função de seus valores atribuidos na instância

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import { Personagem } from "./modules/personagem.js"
 import { PersonagemView } from "./components/personagem-view.js"
 import { Mago } from "./modules/mago.js"
 import { Arqueiro } from "./modules/arqueriro.js"
+import { ArqueiroMago } from "./modules/arqueiro-mago.js"
 
-//Instãncias da class filho, esta que cria também a class pai
+//Instâncias da class filho, esta que cria também a class pai
 const magoAntônio = new Mago('Antônio', 4, 'fogo', 4, 3)
 const magaJulhia = new Mago('Júlia', 8, 'gelo', 7, 10)
 const arqueiroBruno = new Arqueiro('Bruno', 6, 'Ar', 8, 9)
+const arqueiroMagoChico = new ArqueiroMago ('Chico', 7, 10, 'ar', 4, 8)
 
 // array de instâncias que alimenta a função reder no arquivo personagem-view.js
-const personagens = [magoAntônio, magaJulhia, arqueiroBruno]
+const personagens = [magoAntônio, magaJulhia, arqueiroBruno, arqueiroMagoChico]
 
 //Instância o objeto persongemView e já executga a função render.
 new PersonagemView(personagens).render()
-
-const mago = new Mago('manu', 5, 10)
